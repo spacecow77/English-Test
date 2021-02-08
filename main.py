c744df96b3a99fc40fbac50a0fbdffe7e04f6134
spacecow77 <78757912+spacecow77@users.noreply.github.com>
2021-02-08 21:22:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,211 @@
+raspuns_corect = "is reading"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+puncte = 10
+print("")
+print("Pune verbele din paranteza la Prezent Continuu.")
+print("")
+print("Exemplu: My sister (watch)   is watching TV.")
+print("")
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("1) Father _______ (read) the newspaper now.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "is cooking"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("2) She _______ (cook) in the kitchen at the moment.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+
+raspuns_corect = "is snowing"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("3) Look! It _______ (snow).")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "is watching"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("4) My sister _______ (watch) TV.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "are not playing"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("5) The children _______ (not/play) football now.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+
+raspuns_corect = "is sleeping"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("6) He _______ (sleep) at the moment.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "is playing"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("7) Listen! Mother _______ (play) the piano.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "is doing"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("8) Ann _______ (do) her homework.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "am going"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("9) I _______ (go) to church.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+raspuns_corect = "are swimming"
+raspuns_primit = ""
+numar_raspunsuri = 0
+limita_raspunsuri = 3
+limita_depasita = False
+
+
+while raspuns_primit != raspuns_corect and not(limita_depasita):
+    if numar_raspunsuri < limita_raspunsuri:
+        raspuns_primit = input("10) The girls _______ (swim) in the sea now.")
+        numar_raspunsuri = numar_raspunsuri + 1
+    else:
+        limita_depasita = True
+
+if limita_depasita:
+    print("Ai depasit limita de raspunsuri! Pierzi un punct")
+    puncte = puncte -1
+else:
+    print("Felicitari! Nu pierzi niciun punct")
+
+if puncte > 5:
+    print("Felicitari, ai trecut testul cu nota " + str(puncte) + "!")
+else:
+    print("Imi pare rau! Ai picat testul cu nota " + str(puncte) + "!")
